2714073c6bc88527a4579223df5c1ad559f1da6d
varshith <gvarshithreddy8@gmail.com>
2024-04-15 16:04:54 +0530

    added card and welcome components with props

---
+++ 15-04-2023/react-1/my-app/src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './static/Card.css'
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>Card</div>
+    </div>
+    
+  )
+}
+
+export default Card
---
+++ 15-04-2023/react-1/my-app/src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react    '
+
+const Welcome = ({name}) => {
+  return (
+    <div>
+        <h1>
+            Welcome {name}
+        </h1>
+        
+    </div>
+  )
+}
+
+export default Welcome;
