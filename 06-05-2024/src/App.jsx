2aca052300a8a6c5125f3eaa937af9ea0d6248db
gvarshithreddy <gvarshithreddy8@gmail.com>
2024-05-06 15:43:26 +0530

    added presenties tracking state, and update readme

--- 06-05-2024/src/App.jsx
+++ 06-05-2024/src/App.jsx
@@ -1,22 +1,73 @@
 import "./App.css";
 import Card from "./components/Card";
 import NavBar from "./components/NavBar";
-import { useState } from "react";
+import { useState, useContext, createContext } from "react";
 
+const PresentiesContext = createContext();
 function App() {
   const [studentList, setStudentList] = useState([
-    { name: "Varshith", rollno: 160122733176 },
-    { name: "Venu", rollno: 160122733192 },
-    { name: "Abhilash", rollno: 160122733182 },
-    { name: "Bhargav", rollno: 160122733185 },
-    { name: "Bhargav", rollno: 160122733185 },
-    { name: "Bhargav", rollno: 160122733185 },
-    { name: "Bhargav", rollno: 160122733185 },
-    { name: "Bhargav", rollno: 160122733185 },
-    { name: "Bhargav", rollno: 160122733185 },
-    { name: "Bhargav", rollno: 160122733185 },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Varshith",
+      rollno: 160122733176,
+    },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Venu",
+      rollno: 160122733192,
+    },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Abhilash",
+      rollno: 160122733182,
+    },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Bhargav",
+      rollno: 160122733185,
+    },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Nagendra",
+      rollno: 160122733195,
+    },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Pranav",
+      rollno: 160122733175,
+    },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Vijay",
+      rollno: 160122733177,
+    },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Bharath",
+      rollno: 160122733180,
+    },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Vasanth",
+      rollno: 160122733183,
+    },
+    {
+      imageURL: "https://picsum.photos/300/300",
+      name: "Swapnith",
+      rollno: 160122733186,
+    },
   ]);
   const [date, setDate] = useState(new Date());
+  const [courses, setCourses] = useState([
+    "Web Technologies",
+    "Web technologies Lab",
+    "DataBase managemet Systems",
+    "Database Systems Lab",
+    "Probabiility and Statistics",
+  ]);
+
+  const [presenties, setPresenties] = useState([]);
+
   return (
     <>
       <NavBar />
@@ -30,11 +81,25 @@ function App() {
         />
       </div>
 
+      <div className="heading">
+        <label htmlFor="course">For the Course</label>
+        <select name="course" id="course">
+          {courses.map((course, index) => (
+            <option key={index}>{course}</option>
+          ))}
+        </select>
+      </div>
+
       <div className="students">
         {studentList
           .sort((item1, item2) => item1.rollno - item2.rollno)
           .map((student, index) => (
-            <Card key={index} student={student} />
+            <Card
+              key={index}
+              student={student}
+              presenties={presenties}
+              setPresenties={setPresenties}
+            />
           ))}
       </div>
     </>
--- 06-05-2024/src/components/Card.jsx
+++ 06-05-2024/src/components/Card.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-
 import "./static/Card.css";
 const Card = (props) => {
+  const onClickPresent = () => {
+    document.getElementById(props.student.rollno).classList.add("present");
+    let presenties = props.presenties;
+    props.setPresenties([...presenties, props.student.rollno]);
+    console.log(props.presenties);
+  };
+
+  const onClickAbsent = () => {
+    document.getElementById(props.student.rollno).classList.add("absent");
+  };
   return (
-    <div className="card">
-      <img src="https://picsum.photos/300/300" alt="random" />
+    <div className="card" id={props.student.rollno}>
+      <img src={props.student.imageURL} alt="random" />
       <div>
         <h2>{props.student.rollno}</h2>
         <h3>{props.student.name}</h3>
       </div>
       <div className="footer">
-        <button>Present</button>
-        <button>Absent</button>
+        <button onClick={onClickPresent}>Present</button>
+        <button onClick={onClickAbsent}>Absent</button>
       </div>
     </div>
   );
